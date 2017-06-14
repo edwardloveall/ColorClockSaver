a0195aaeeeaf7b0ad283743966ea42c1aebf3b5a
Edward Loveall <edward@edwardloveall.com>
2017-06-14 16:24:00 -0400

    Draw the time
    This uses a date formatter to get a hour:minute:second from the current
    date. It also uses a layout manager to draw that string on screen. I
    can't say that I really understand the layout manager part yet.

--- MainView.swift
+++ MainView.swift
@@ -2,7 +2,22 @@ import ScreenSaver
 
 class MainView: ScreenSaverView {
   override func draw(_ rect: NSRect) {
-    NSColor.red.setFill()
-    NSBezierPath.fill(rect)
+    let date = Date()
+    let formatter = DateFormatter()
+    formatter.dateFormat = "HH:mm:ss"
+    let dateString = formatter.string(from: date)
+    let storage = NSTextStorage(string: dateString)
+    let layoutManager = NSLayoutManager()
+    let textContainer = NSTextContainer()
+    layoutManager.addTextContainer(textContainer)
+    storage.addLayoutManager(layoutManager)
+    let glyphRange = layoutManager.glyphRange(for: textContainer)
+    self.lockFocus()
+    layoutManager.drawGlyphs(forGlyphRange: glyphRange, at: bounds.origin)
+    self.unlockFocus()
+  }
+
+  override func animateOneFrame() {
+    setNeedsDisplay(bounds)
   }
 }
