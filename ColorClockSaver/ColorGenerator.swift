450891e4411af45a2645ba3d548bc4f063c5fe80
Edward Loveall <edward@edwardloveall.com>
2017-06-16 22:53:47 -0400

    Heavy refactor of the ColorGengerator
    When I tried to make colors come out, they were 100% white. This is
    because NSColor uses values from 0 - 1, not 0 - 255. So this is a
    refactor to turn the base method `rgbComponentsFromTime` to return
    values from 0 - 1. This unfortunately makes the `hexColorFromTime`
    method a bit more complicated because I have to convert everything to 0
    - 255 as Ints.

--- ColorClockSaver/ColorGenerator.swift
+++ ColorClockSaver/ColorGenerator.swift
@@ -1,29 +1,33 @@
 import Cocoa
 
 class ColorGenerator {
-  func rgbComponentsFromTime() -> (red: Int, green: Int, blue: Int) {
+  func rgbComponentsFromTime() -> (red: Double, green: Double, blue: Double) {
     let calendar = Calendar(identifier: .gregorian)
     let date = Date()
     let hour = calendar.component(.hour, from: date)
     let minute = calendar.component(.minute, from: date)
     let second = calendar.component(.second, from: date)
 
-    let hexHour = (255.0/23) * Double(hour)
-    let hexMinute = (255.0/59) * Double(minute)
-    let hexSecond = (255.0/59) * Double(second)
+    let hexHour = (1.0/23) * Double(hour)
+    let hexMinute = (1.0/59) * Double(minute)
+    let hexSecond = (1.0/59) * Double(second)
 
     return (
-      red: Int(hexHour),
-      green: Int(hexMinute),
-      blue: Int(hexSecond)
+      red: hexHour,
+      green: hexMinute,
+      blue: hexSecond
     )
   }
 
   func hexColorFromTime() -> String {
     let rgbValues = rgbComponentsFromTime()
-    let hexRed = String(format: "%02x", rgbValues.red)
-    let hexGreen = String(format: "%02x", rgbValues.green)
-    let hexBlue = String(format: "%02x", rgbValues.blue)
+    let scaledRed = Int(rgbValues.red * 255)
+    let scaledGreen = Int(rgbValues.green * 255)
+    let scaledBlue = Int(rgbValues.blue * 255)
+
+    let hexRed = String(format: "%02x", scaledRed)
+    let hexGreen = String(format: "%02x", scaledGreen)
+    let hexBlue = String(format: "%02x", scaledBlue)
 
     return "\(hexRed)\(hexGreen)\(hexBlue)".uppercased()
   }
