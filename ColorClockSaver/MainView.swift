2bdb128759a54992352fae95a92250b81729df48
Edward Loveall <edward@edwardloveall.com>
2017-06-23 15:57:18 -0400

    Refactor structure in the main view
    I am doing my best to simplify, clarify, and move logic around to
    better spots. This created a new method called configureViews() which
    adds views to the view hierarchy and sets any attributes that they need.
    
    layoutViews() can then focus on purely auto layout code.

--- ColorClockSaver/MainView.swift
+++ ColorClockSaver/MainView.swift
@@ -9,29 +9,33 @@ class MainView: ScreenSaverView {
   override func viewDidMoveToWindow() {
     wantsLayer = true
     animationTimeInterval = 1
-    Fonts.load(fontName: Fonts.timeFont, extension: "ttf")
+
+    configureViews()
     layoutViews()
+
+    Fonts.load(fontName: Fonts.timeFont, extension: "ttf")
     timeView.resizeFont(for: bounds.size)
     colorCodeView.resizeFont(for: bounds.size)
   }
 
-  func layoutViews() {
-    addSubview(backgroundView)
-    backgroundView.translatesAutoresizingMaskIntoConstraints = false
-
-    backgroundView.topAnchor.constraint(equalTo: topAnchor).isActive = true
-    backgroundView.heightAnchor.constraint(equalTo: heightAnchor).isActive = true
-    backgroundView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-    backgroundView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
-
+  func configureViews() {
     wrapperView.alignment = .centerX
     wrapperView.orientation = .vertical
     wrapperView.distribution = .equalCentering
     wrapperView.addArrangedSubview(timeView)
     wrapperView.addArrangedSubview(colorCodeView)
+    addSubview(backgroundView)
     addSubview(wrapperView)
+  }
 
+  func layoutViews() {
+    backgroundView.translatesAutoresizingMaskIntoConstraints = false
     wrapperView.translatesAutoresizingMaskIntoConstraints = false
+
+    backgroundView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+    backgroundView.heightAnchor.constraint(equalTo: heightAnchor).isActive = true
+    backgroundView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+    backgroundView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
 
     wrapperView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
     wrapperView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
